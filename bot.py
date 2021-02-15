97eb837cb0908f26984549baf8dbc023fbaa9972
WUMPUS9999 <77828524+WUMPUS9999@users.noreply.github.com>
2021-02-15 21:31:12 +0545

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-client=commands.Bot(command_prefix='!')
+client=commands.Bot(command_prefix='-')
 @client.event
 async def on_ready():
     print("Bot is ready")
